2d18bbab9edd8bf6149ec7d31e99656058f5298d
babakpst <babakp@utexas.edu>
2018-01-22 09:26:43 -0600

    adding classes

---
+++ Shape_Function_Class.py
@@ -0,0 +1,23 @@
+
+#####################################################################
+# Code developed by: Dr. Babak Poursartip
+# Supervised by:     Dr. Clint Dawson
+# The Institute for Computational Engineering and Sciences (ICES)
+# The University of Texas at Austin
+#
+# Start date:    01/22/2018
+# Latest update: 01/22/2018
+#
+# Comment: This class specifies the shape functions
+#
+#####################################################################
+
+
+class Shape_Function_Class:
+
+    def __int__(self)
+        pass
+
+    def Element2D_4N(self)
+
+        
